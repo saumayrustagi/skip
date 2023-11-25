a5c0bc92db5821cf06102b01332fb0d728340d59
Saumay Rustagi <saumay03pro@gmail.com>
2023-11-26 02:59:36 +0530

    Modify background.js Add rudimentary blocking Changed function names

--- background.js
+++ background.js
@@ -1,52 +1,48 @@
-function listenAndSkipAds() {
-  skipAds();
-  setTimeout(() => {
-    listenAndSkipAds()
-  }, 100);
+const AD_SKIP_TIME = 99999;
+
+let isSkipping = false;
+function checkAndSkip(parentWrapper, htmlVideoContainer) {
+	if (isSkipping) return;
+	if (String(parentWrapper.className).includes("ad-showing")) {
+		isSkipping = true;
+		const element = htmlVideoContainer.children[0]
+		if (element.tagName === "VIDEO") {
+			const videoPlayer = element;
+			if (videoPlayer.currentTime != AD_SKIP_TIME) videoPlayer.currentTime = AD_SKIP_TIME;
+			console.log("Ad Skipped!");
+			const ytButtons = Array.from(document.getElementsByClassName("ytp-button"));
+			const skipButtons = ytButtons.filter(button => button.className.includes('skip'));
+			if (skipButtons.length > 0) {
+				skipButtons[0].click();
+			}
+		}
+		isSkipping = false
+	}
+	setTimeout(() => { checkAndSkip(parentWrapper, htmlVideoContainer) }, 500)
 }
 
+// let blocker = false;
 function skipAds() {
-  const htmlVideoContainers = Array.from(document.getElementsByClassName("html5-video-container"));
-  if (htmlVideoContainers.length === 0) {
-    console.log("skip: no video containers found");
-  }
-  else if (htmlVideoContainers.length > 1) {
-    console.log("skip: multiple video containers found?")
-  }
-  else {
-    console.log("skip: video container found")
-    const htmlVideoContainer = htmlVideoContainers[0];
-    const parentWrapper = htmlVideoContainer.parentNode;
-    console.log("skip: " + parentWrapper);
-    if (parentWrapper !== null || parentWrapper !== undefined) {
-      console.log("skip: checking if ad is playing...")
-         if (String(parentWrapper.className).includes("ad-showing")) {
-          console.log("skip: ad is playing, trying to skip...")
-          Array.from(htmlVideoContainer.children).forEach((element) => {
-            if (element.tagName === "VIDEO") {
-              console.log("skip: video player found")
-              const videoPlayer = element;
-              videoPlayer.currentTime = 99999;
-              const skipAdButtons = Array.from(document.getElementsByClassName("ytp-ad-skip-button ytp-button"));
-              if (skipAdButtons.length > 0) {
-                skipAdButtons[0].click();
-              } 
-            }
-          });
-         }
-    }
-    else {
-      console.log("skip: else?")
-    }
-  }
+	// if (blocker) return;
+	// blocker = true;
+	const htmlVideoContainers = Array.from(document.getElementsByClassName("html5-video-container"));
+	if (htmlVideoContainers.length === 1) {
+		const htmlVideoContainer = htmlVideoContainers[0];
+		const parentWrapper = htmlVideoContainer.parentNode;
+		if (parentWrapper) {
+			checkAndSkip(parentWrapper, htmlVideoContainer)
+		}
+	}
+	// blocker = false
+	// setTimeout(() => { skipAds() }, 1000);
 }
 
 chrome.tabs?.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status === "complete" && String(tab.url).includes("https://www.youtube.com/watch")) {
-    console.log("youtube video loaded");
-    chrome.scripting.executeScript({
-            target: { tabId: tabId },
-            function: listenAndSkipAds,
-          }); 
-  }
+	if (changeInfo.status === "complete" && String(tab.url).includes("https://www.youtube.com/watch")) {
+		console.log("youtube video loaded");
+		chrome.scripting.executeScript({
+			target: { tabId: tabId },
+			function: skipAds,
+		});
+	}
 })
